afc24d1540e30143e97b28e79595d673fb2c8401
Matt <mattparsels@yahoo.com>
2023-04-11 12:33:36 -0400

    navbar with icons

--- frontend/src/AppNavBar.js
+++ frontend/src/AppNavBar.js
@@ -8,19 +8,28 @@ const AppNavBar = () => {
   return (
     <Navbar bg="dark" variant='dark' export="lg">
       <Container>
-        <Navbar.Brand href="#home">Welcome to Wright-Patterson AFB</Navbar.Brand>
+        <Navbar.Brand href="/">Welcome to Wright-Patterson AFB</Navbar.Brand>
         <Nav className="justify-content-end">
           <Nav.Item>
-            <Nav.Link href="#home">support</Nav.Link>
+            <Nav.Link href="#home"><svg className="navIcon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-info-circle" viewBox="0 0 16 16">
+              <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+              <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z" />
+            </svg> support</Nav.Link>
+
           </Nav.Item>
-          <Nav.Item>
+          {/* <Nav.Item>
             <Nav.Link href="#map">Map</Nav.Link>
+          </Nav.Item> */}
+          <Nav.Item>
+            <Nav.Link href="/Details"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+              <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
+            </svg> Profile</Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link href="#profile">Profile</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link href="#Logout">Logout</Nav.Link>
+            <Nav.Link href="#Logout"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="red" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z" />
+              <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z" />
+            </svg> Logout</Nav.Link>
           </Nav.Item>
         </Nav>
       </Container>
