3e5316be61aa47cea93baf895314b4fed335b0f4
Adrian <lewisadrian5213@gmail.com>
2023-04-13 13:51:02 -0600

    map files

--- frontend/src/components/Details/Map.js
+++ frontend/src/components/Details/Map.js
@@ -1,26 +1,18 @@
 import React, { useRef, useEffect, useState } from 'react';
-import mapboxgl from 'mapbox-gl'; 
-import {Container, Row, Col, Button} from 'react-bootstrap';
+import mapboxgl from 'mapbox-gl';
+import { Container, Row, Col, Button } from 'react-bootstrap';
 import './Map.css'
 
 // public access token
 mapboxgl.accessToken = 'pk.eyJ1IjoiYWxwYWw1MjEzIiwiYSI6ImNsZ2U3aDNzZzJoeDUzZWxpMHlzNHdseGkifQ.0SJDwmo3e81ANVKxx-nQ1A';
 
-const selectedLocation = {
-  latitude: 39.7827, 
-  longitude: -84.1040, 
-  building: 'National Museum of the United States Air Force', 
-  room: 1, address: '1100 Spaatz St, Dayton, OH 45433', 
-  phone_number: '937-255-3286', 
-  hours: '9:00 a.m. –5 p.m. M-F', 
-  url: 'https://www.nationalmuseum.af.mil/'
-}
-
 // Change input argument to selectedLocation for final
-const Map = ({selectedLoc}) => {
+const Map = ({ selectedLocation }) => {
   const mapContainer = useRef(null);
   const map = useRef(null);
-  const [locations, setLocations] = useState(null)
+  const [locations, setLocations] = useState(null);
+  const [style, setStyle] = useState(false);
+  const [marker, setMarker] = useState(null);
 
   const handleHome = () => {
     map.current.flyTo({
@@ -29,7 +21,7 @@ const Map = ({selectedLoc}) => {
       bearing: 0,
       pitch: 0,
       essential: true
-      });
+    });
   }
 
   //useEffect to get locations from server.
@@ -38,7 +30,7 @@ const Map = ({selectedLoc}) => {
       .then(res => res.json())
       .then(data => setLocations(data))
       .catch(error => console.log("Error fetching data: ", error))
-    }, []);
+  }, []);
 
   //create map
   useEffect(() => {
@@ -46,48 +38,58 @@ const Map = ({selectedLoc}) => {
 
     map.current = new mapboxgl.Map({
       container: mapContainer.current,
-      style: 'mapbox://styles/mapbox/outdoors-v12',
+      style: style ? 'mapbox://styles/mapbox/navigation-night-v1' : 'mapbox://styles/mapbox/outdoors-v12',
       center: [-84.0537, 39.8137],
       zoom: 12
     })
-    .addControl(new mapboxgl.FullscreenControl())
-    .addControl(new mapboxgl.NavigationControl())
+      .addControl(new mapboxgl.FullscreenControl(), 'top-right')
+      .addControl(new mapboxgl.NavigationControl(), 'top-right')
 
-    const markers = locations.map(location => new mapboxgl.Marker({draggable: false})
-      .setLngLat([location.longitude, location.latitude])
-      .setPopup(new mapboxgl.Popup()
-      .setText(`${location.building}`))
-      .addTo(map.current))
-    }, [locations]);
+
+  }, [locations, style]);
 
   // map fly to launch site when launch selected on home page
-  useEffect(()=>{
-    if(!selectedLocation || !locations) return;
-
-    let ID = selectedLocation.id
-    let copy = [...locations]
-    let result = copy.filter(location => location.id===ID)
+  useEffect(() => {
+    if (!selectedLocation || !locations) return;
 
     map.current.flyTo({
       center: [selectedLocation.longitude, selectedLocation.latitude],
-      zoom: 13,
-      duration: 5000,
-      bearing: 100,
-      pitch: 75,
+      zoom: 16,
+      duration: 0,
+      bearing: 0,
+      pitch: 0,
       essential: true
     });
+
+    // clear previous map markers
+    if(marker) {marker.remove();}
+    
+    setMarker(new mapboxgl.Marker({ draggable: false })
+      .setLngLat([selectedLocation.longitude, selectedLocation.latitude])
+      .setPopup(new mapboxgl.Popup().setText(`${selectedLocation.building}`))
+      .addTo(map.current))
   }, [selectedLocation])
 
-  return (
+  const changeStyle = (e) => {
+    e.preventDefault();
+    setStyle(!style);
+  }
 
+  return (
     <Container className='py-2 map-wrapper'>
       <Row className='justify-content-center'>
         <div ref={mapContainer} className="map-container" />
       </Row>
-      <Button onClick={()=>handleHome()}variant='light' className='reset-btn'>Home</Button>
+      <Row>
+        <Col>
+          <Button onClick={() => handleHome()} variant='success' className='reset-btn'>Home</Button>
+        </Col>
+        <Col>
+          <Button onClick={changeStyle} variant='primary' className='style-btn'>Change Theme</Button>
+        </Col>
+      </Row>
     </Container>
-      
   );
 }
- 
+
 export default Map;
