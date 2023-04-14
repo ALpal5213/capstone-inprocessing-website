1bef87e407589fd8379e93e466eb413a1b5f0469
Adrian <lewisadrian5213@gmail.com>
2023-04-14 10:41:46 -0600

    styling for table and background

--- frontend/src/components/Home/HomePage.js
+++ frontend/src/components/Home/HomePage.js
@@ -7,7 +7,7 @@ export const HomePage = () => {
 
     return (
         <>
-            <AddTask />
+            {/* <AddTask /> */}
             {/* <ProgressBar /> */}
             <TaskTabs />
         </>
