fe5d26abb3507d3fe7c916d4734011c6452e9f5b
Adrian <lewisadrian5213@gmail.com>
2023-04-17 14:46:54 -0600

    updated announcements and changed data type of columns in tasks and announcements migrations from string to text

--- backend/seeds/06_initial_Announcements.js
+++ backend/seeds/06_initial_Announcements.js
@@ -7,7 +7,7 @@ exports.seed = async function(knex) {
   await knex('Announcements').del()
   await knex('Announcements').insert([
     {announcement: 'Building 270 is closed today', image: './announce-icon.png'},
-    {announcement: 'WPAFB Visitor Center will be closed for training on April 26th', image: './announce-icon.png'},
+    {announcement: 'Gate 01 Visitor Center will be closed for training on April 26th. Please use the Gate 02 Visitor Center. Extra text to display what a really long announcement will look like in this section. Scroll to see additional announcement text or just do not write really long announcements', image: './announce-icon.png'},
     {announcement: 'Finance Office hours will be from 10 a.m. to 2 p.m. on April 28th', image: './announce-icon.png'},
     {announcement: 'Base operations limited on Memorial Day', image: './announce-icon.png'},
     {announcement: 'Personnel Office closed for training on April 28th', image: './announce-icon.png'},
