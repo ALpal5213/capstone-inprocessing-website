34a9f8d7ac34206c0cea6b1417c3eccee3ff5a63
brandonLee <brandonlee196582@gmail.com>
2023-04-17 13:39:26 -0600

    git fix

--- frontend/src/components/Home/HomePage.js
+++ frontend/src/components/Home/HomePage.js
@@ -1,22 +1,15 @@
 import AppNavBar from "../NavBar/AppNavBar"
-
+import AddTask from "./AddTask"
 import {TaskTabs} from "./TaskTable"
 
 export const HomePage = () => {
 
     return (
         <>
-<<<<<<< HEAD
-<<<<<<< HEAD
             {/* <AppNavBar/> */}
-=======
             <AddTask />
             {/* <ProgressBar /> */}
->>>>>>> 1ac029f7ded88c02c013561e9673387261f925bd
-=======
             {/* <AddTask /> */}
-            
->>>>>>> 27aae3e057fabfdbb7c25d8e74c29d5c7b5403e3
             <TaskTabs />
         </>
     )
