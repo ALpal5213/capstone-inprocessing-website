500638772cfffaa81aa91949e90c7fcc90584a8c
brandonLee <brandonlee196582@gmail.com>
2023-04-19 19:04:07 -0600

    added filter and sort functionality

--- frontend/src/components/ManageAllTasks/ManageAllTasks.js
+++ frontend/src/components/ManageAllTasks/ManageAllTasks.js
@@ -9,24 +9,47 @@ import { TaskModify } from '../TaskModify/TaskModify'
 import { useNavigate } from 'react-router-dom';
 import { useContext } from 'react';
 import { GlobalContext } from '../../App';
+import Form from 'react-bootstrap/Form';
 
 export const ManageAllTasks = () => {
 
-  const { modifyTableShow, setModifyTableShow,  modifyTableQuery, setmodifyTableQuery } = useContext(GlobalContext)
-  const navigate = useNavigate();
+  const { setModifyTableShow, setmodifyTableQuery } = useContext(GlobalContext)
   const [allTasks, setAllTasks] = useState();
   const [filteredTasks, setFilteredTasks] = useState();
-  const [tableData, setTableData] = useState();
   const [newTableData, setNewTableData] = useState();
   const [tablePage, setTablePage] = useState(1);
   const [pageIndex, setPageIndex] = useState();
-  
-  const [idSort, setIdSort] = useState('a');
-  const [idSortIcon, setIdSortIcon] = useState(
+
+  const upIcon = (
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chevron-up" viewBox="0 0 16 16">
+      <path fillRule="evenodd" d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z"/>
+    </svg>
+  )
+  const downIcon = (
     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" classNmae="bi bi-chevron-down" viewBox="0 0 16 16">
       <path fillRule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
     </svg>
-)
+  )
+  const dashIcon = (
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-dash-lg" viewBox="0 0 16 16">
+      <path fillRule="evenodd" d="M2 8a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11A.5.5 0 0 1 2 8Z"/>
+    </svg>
+  )
+  
+  const [idSort, setIdSort] = useState('a');
+  const [idSortIcon, setIdSortIcon] = useState(downIcon)
+  const [taskNameSort, setTaskNameSort] = useState('d');
+  const [taskNameSortIcon, setTaskNameSortIcon] = useState(dashIcon)
+  const [taskTypeSort, setTaskTypeSort] = useState('d');
+  const [taskTypeSortIcon, setTaskTypeSortIcon] = useState(dashIcon)
+  const [fullnameSort, setFullnameSort] = useState('d');
+  const [fullnameSortIcon, setFullnameSortIcon] = useState(dashIcon)
+  const [dueDateSort, setDueDateSort] = useState('a');
+  const [dueDateSortIcon, setDueDateSortIcon] = useState(dashIcon)
+  const [statusSort, setStatusSort] = useState('a');
+  const [statusSortIcon, setStatusSortIcon] = useState(dashIcon)
+
+
   const pageButtons = (
     <div className='pageCtrlDiv'>
       <button className='pageCtrlBtn pageCtrlBtnLeft' onClick={() => firstPageControl()}>
@@ -56,39 +79,13 @@ export const ManageAllTasks = () => {
       </button>
     </div>
   )
-
-  const columns = [
-    { dataField: 'task_name', text: '1' },
-    { dataField: 'task_description', text: '2' }
-  ];
-
-  const options = {
-    paginationSize: 15,
-    pageStartIndex: 0,
-    firstPageText: 'First',
-    prePageText: 'Back',
-    nextPageText: 'Next',
-    lastPageText: 'Last',
-    nextPageTitle: 'First page',
-    prePageTitle: 'Pre page',
-    firstPageTitle: 'Next page',
-    lastPageTitle: 'Last page',
-    sizePerPageList: [{
-      text: 'show 15', value: 15
-    }, {
-      text: 'show 30', value: 30
-    }, {
-      text: 'Show all', value: allTasks ? allTasks.length : 0
-    }]
-  };
-
   
   useEffect(() => {
     fetch(`http://localhost:3001/tasks-users`)
     .then(res => res.json())
     .then(tasks => {
       setAllTasks(tasks)
-      setFilteredTasks(tasks)
+      setFilteredTasks(tasks.filter(task => task.status !== 'complete'))
     })
   }, [])
   
@@ -168,24 +165,50 @@ export const ManageAllTasks = () => {
     setPageIndex(filteredTasks.length - lastPageSize)
   }
 
-  const sortById = (event) => {
-    var idSortedTasks;
-    if (idSort === 'a') {
-      setIdSortIcon(
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chevron-up" viewBox="0 0 16 16">
-          <path fillRule="evenodd" d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z"/>
-        </svg>
-      )
-      idSortedTasks = filteredTasks.sort((a, b) => {return b.task_id - a.task_id});
-      setIdSort('d')
+  const sortByCol = (setIconFunction, setSortFunction, sortDir, col, sortType) => {
+    if (col === 'task_id') {
+      setTaskNameSortIcon(dashIcon)
+      setTaskNameSort('d')
+      setTaskTypeSortIcon(dashIcon)
+      setTaskTypeSort('d')
+      setFullnameSortIcon(dashIcon)
+      setFullnameSort('d')
+      setDueDateSortIcon(dashIcon)
+      setDueDateSort('a')
+      setStatusSortIcon(dashIcon)
+      setStatusSort('a')
     } else {
-      setIdSortIcon(
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" classNmae="bi bi-chevron-down" viewBox="0 0 16 16">
-          <path fillRule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
-        </svg>
-      )
-      idSortedTasks = filteredTasks.sort((a, b) => {return a.task_id - b.task_id});
-      setIdSort('a')
+      setIdSortIcon(dashIcon)
+      setIdSort('d')
+    }
+    if (sortDir === 'a') {
+      setIconFunction(upIcon)
+      if (sortType === 'num') {
+        filteredTasks.sort((a, b) => {return b[col] - a[col]});
+      } 
+      if (sortType === 'string') {
+        filteredTasks.sort((a, b) => {
+          let fa = a[col].toLowerCase(), fb = b[col].toLowerCase();
+          if (fa > fb) return -1;
+          if (fa < fb) return 1;
+          return 0;
+        });
+      }
+      setSortFunction('d')
+    } else {
+      setIconFunction(downIcon)
+      if (sortType === 'num') {
+        filteredTasks.sort((a, b) => {return a[col] - b[col]});
+      }
+      if (sortType === 'string') {
+        filteredTasks.sort((a, b) => {
+          let fa = a[col].toLowerCase(), fb = b[col].toLowerCase();
+          if (fa < fb) return -1;
+          if (fa > fb) return 1;
+          return 0;
+        });
+      }
+      setSortFunction('a')
     }
     if (pageIndex > 0) {
       setPageIndex(0)
@@ -199,31 +222,91 @@ export const ManageAllTasks = () => {
     setTablePage(1)
   }
 
+  const filterBy = () => {
+    var items = allTasks;
+    if (document.getElementById('task_id').value) {
+      items = items.filter(task => `${task['task_id']}`.match(document.getElementById('task_id').value))
+    }
+    if (document.getElementById('task_name').value) {
+      items = items.filter(task => task['task_name'].toLowerCase().match(document.getElementById('task_name').value.toLowerCase()))
+    }
+    if (document.getElementById('task_type').value) {
+      items = items.filter(task => task['task_type'].toLowerCase().match(document.getElementById('task_type').value.toLowerCase()))
+    }
+    if (document.getElementById('fullname').value) {
+      items = items.filter(task => task['fullname'].toLowerCase().match(document.getElementById('fullname').value.toLowerCase()))
+    }
+    if (document.getElementById('due_date').value) {
+      items = items.filter(task => task['due_date'].match(`${document.getElementById('due_date').value}`))
+    }
+    if (document.getElementById('status').value) {
+      items = items.filter(task => task['status'].toLowerCase().match(document.getElementById('status').value.toLowerCase()))
+    }
+    
+    
+    setFilteredTasks(items)
+    setNewTableData(
+      items.map((task, i) => {
+        return (
+          i > pageIndex - 1 ?
+            i < pageIndex + 30 ?
+              <tr className="task tableCol" key={i} id={task.id} onClick={handleModifyShow}>
+                <td>
+                  {task.task_id}
+                </td>
+                <td>
+                  {task.task_name}
+                </td>
+                <td>
+                  {task.task_type}
+                </td>
+                <td>
+                  {task.fullname}
+                </td>
+                <td>
+                  {task.due_date}
+                </td>
+                <td>
+                  {task.status}
+                </td>
+              </tr> :
+              '' :
+            ''
+        )
+      })
+    )
+  }
+
   return(
     <>
       <div className='allTaskTableDiv'>
         {pageButtons}
-        <Table hover className="taskTable">
+        <Table hover className="taskTable table-fixed text-nowrap">
           <thead>
             <tr className='tableCol'>
-              <th>
-                Task Id
-                <button id='sortIdButton' className='sortButton' onClick={(event) => sortById(event)}>{idSortIcon}</button>
+              <th className='idCol'>
+                <input id='task_id' className='filterBox idBox' type="text" placeholder="Id" onChange={(event) => filterBy(event.target.value, event.target.id)}/>
+                <button className='sortButton' onClick={() => sortByCol(setIdSortIcon, setIdSort, idSort, 'task_id','num')}>{idSortIcon}</button>
               </th>
-              <th>
-                Task
+              <th className='taskCol'>
+                <input id='task_name' className='filterBox taskBox' type="text" placeholder="Task" onChange={(event) => filterBy(event.target.value, event.target.id)}/>
+                <button className='sortButton' onClick={() => sortByCol(setTaskNameSortIcon, setTaskNameSort, taskNameSort, 'task_name', 'string')}>{taskNameSortIcon}</button>
               </th>
-              <th>
-                Type
+              <th className='typeCol'>
+                <input id='task_type' className='filterBox typeBox' type="text" placeholder="Type" onChange={(event) => filterBy(event.target.value, event.target.id)}/>
+                <button className='sortButton' onClick={() => sortByCol(setTaskTypeSortIcon, setTaskTypeSort, taskTypeSort, 'task_type', 'string')}>{taskTypeSortIcon}</button>
               </th>
-              <th>
-                User
+              <th className='userCol'>
+                <input id='fullname' className='filterBox userBox' type="text" placeholder="User" onChange={(event) => filterBy(event.target.value, event.target.id)}/>
+                <button className='sortButton' onClick={() => sortByCol(setFullnameSortIcon, setFullnameSort, fullnameSort, 'fullname', 'string')}>{fullnameSortIcon}</button>
               </th>
-              <th>
-                Due
+              <th className='dueByCol'>
+                <input id='due_date' className='filterBox dueByBox' type="text" placeholder="Due By" onChange={(event) => filterBy(event.target.value, event.target.id)}/>
+                <button className='sortButton' onClick={() => sortByCol(setDueDateSortIcon, setDueDateSort, dueDateSort, 'due_date', 'string')}>{dueDateSortIcon}</button>
               </th>
-              <th>
-                Status
+              <th className='statusCol'>
+              <input id='status' className='filterBox statusBox' type="text" placeholder="Status" onChange={(event) => filterBy(event.target.value, event.target.id)}/>
+                <button className='sortButton' onClick={() => sortByCol(setStatusSortIcon, setStatusSort, statusSort, 'status', 'string')}>{statusSortIcon}</button>
               </th>
             </tr>
           </thead>
